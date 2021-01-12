88870fa397498382bf9d5124ab20489c0ca3a028
Youngeun <Youngeun@DESKTOP-11E0KUI>
2021-01-12 17:41:12 +0900

    Hello world.java

---
+++ sample_github1/src/com/kigdata/edu/HelloWord.java
@@ -0,0 +1,30 @@
+package com.kigdata.edu;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hw") // 요청 URL 매핑: 요청이 들어오면 /hw로 보냄
+public class HelloWord extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public HelloWord() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Hello world!");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
