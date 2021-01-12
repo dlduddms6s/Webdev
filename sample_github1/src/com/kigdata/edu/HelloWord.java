fbe58c3a8ec10dc5b313b9e0b9a44c6f0b467b5e
Youngeun <Youngeun@DESKTOP-11E0KUI>
2021-01-12 17:43:08 +0900

    java

--- sample_github1/src/com/kigdata/edu/HelloWord.java
+++ sample_github1/src/com/kigdata/edu/HelloWord.java
@@ -19,6 +19,8 @@ public class HelloWord extends HttpServlet {
 			throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		System.out.println("Hello world!");
+		System.out.println("Hello world!");
+
 		
 	}
 
